1178e9121ee9667860a2536e2fe204db0273db89
Kevin Bruland <kevinmbruland@gmail.com>
2019-05-12 13:03:00 -0400

    Add user initial testing setup

--- routes/api.js
+++ routes/api.js
@@ -9,41 +9,93 @@ const slackToken = process.env.SLACK_TOKEN;
 // Endpoint to lookup a slack user ID based on username
 // Test example: api/getSlackUserId?username=kevinmbruland 
 // TODO: Verify the slack app API really can't lookup user info by username?
-router.get('/getSlackUserId', function (req, res, next) {
 
+function checkSlackUsername(slackUsername) {
+  return axios.get(slackUserListEndpoint + '?token=' + slackToken)
+    .then(function (res) {
+      const slackMembers = res.data.members;
+      for (let i = 0; i < slackMembers.length; i++) {
+        if (slackUsername === slackMembers[i].name) {
+          return slackMembers[i];
+        }
+      }
+      return null;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+router.get('/getSlackUserId', function (req, res, next) {
   const reqName = req.query.username;
 
   if (reqName) {
-    axios.get(slackUserListEndpoint + '?token=' + slackToken)
-      .then(function (slackReq) {
-        const slackMembers = slackReq.data.members;
-        for (let i = 0; i < slackMembers.length; i++) {
-          if (reqName === slackMembers[i].name) {
-            return res.json({ slackId: slackMembers[i].id });
-          }
-        }
+    checkSlackUsername(reqName).then(function (slackUserInfo) {
+      if (slackUserInfo) {
+        res.json({ slackId: slackUserInfo.id });
+      } else {
         res.json({ error: 'No slack username, ' + reqName + ', found' });
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
+      }
+      res.end();
+    });
   } else {
-    res.json(
-      { error: 'No name value provided' }
-    )
+    res.json({ error: 'No username provided' });
+    res.end();
   }
 });
 
-router.post('/addNewUser', function (req, res, next) {
-  if (req.body.username) {
-    for (let i = 0; i < mockUserData.length; i++) {
-      if (req.body.username === mockUserData[i].name) {
-        res.json({ userNotification: 'Username already exists!' });
-        return res.end();
+function checkIfUserExists(name) {
+  for (let i = 0; i < mockUserData.length; i++) {
+    if (name === mockUserData[i].name) {
+      return mockUserData[i];
+    }
+  }
+  return null;
+}
+
+function checkIfPlatformUserExists(platformName, username) {
+  for (let i = 0; i < mockUserData.length; i++) {
+    for (let j = 0; j < mockUserData[i].platforms.length; j++) {
+      if (mockUserData[i].platforms[j].platformName === platformName) {
+        if (mockUserData[i].platforms[j].username === username) {
+          return true;
+        }
       }
     }
-    res.json({userNotification: 'User successfully added!'});
-    res.end();
+  }
+  return null;
+}
+
+router.post('/addNewUser', function (req, res, next) {
+  console.log(req.body);
+  const newUsername = req.body.username;
+  const chatPlatform = req.body.chatPlatform;
+  const platformUsername = req.body.platformUsername;
+  const userData = checkIfUserExists(newUsername);
+  let platformId;
+
+  if (userData) {
+    res.json({ userNotification: 'Username already exists!' });
+    return res.end();
+  }
+
+  if (checkIfPlatformUserExists(chatPlatform, platformUsername)) {
+    res.json({ userNotification: chatPlatform + ' username already attached to a user!' });
+    return res.end();
+  };
+
+  if(chatPlatform === "slack") {
+    checkSlackUsername(platformUsername).then(function(data){
+      if(data) {
+        platformId = data.id;
+        res.json({ userNotification: 'User successfully added, Slack ID: ' + platformId });
+        return res.end();
+      }
+      else {
+        res.json({ userNotification: 'Unable to lookup Slack information!' });
+        return res.end();
+      }
+    });
   }
 });
 
